58d5223ce2c81ac97a1153610a27e2613f878af4
Qile Wang <wangqile123@gmail.com>
2015-12-05 18:07:15 -0500

    Initial Commit

---
+++ polls/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import url
+
+from . import views
+
+# app_name to define subpages for django in case of multiple apps
+#app_name = 'polls'
+#urlpatterns = [
+#	url(r'^$', views.index, name='index'),
+#	# Polls/question_id
+#	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
+#	# Polls/question_id/results
+#	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name="results"),
+#	# Polls/question_id/vote
+#	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+#]
+app_name = 'polls' # needed even with generic views!
+urlpatterns = [
+	url(r'^$', views.IndexView.as_view(), name='index'),
+	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
